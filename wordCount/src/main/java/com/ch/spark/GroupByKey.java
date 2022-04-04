9dfd2fdb453ab067ed8f1148ec7bd6b949c57cd4
chenchen <cc.chen.chen@foxmail.com>
2022-04-04 21:48:08 +0800

    commit

--- wordCount/src/main/java/com/ch/spark/GroupByKey.java
+++ wordCount/src/main/java/com/ch/spark/GroupByKey.java
@@ -2,11 +2,15 @@ package com.ch.spark;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.VoidFunction;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 
 public class GroupByKey {
     public static void main(String[] args) {
@@ -30,6 +34,31 @@ public class GroupByKey {
                 new Tuple2<>("wanWu", 78),
                 new Tuple2<>("maLiu", 78)
         ));
+
+//        countByKey按key计数，countByValue按值计数
+        Map<String, Long> stringLongMap = rdd.countByKey();
+        Set<Map.Entry<String, Long>> entries = stringLongMap.entrySet();
+        for(Map.Entry<String, Long> e:entries){
+            String key = e.getKey();
+            Long value = e.getValue();
+            System.out.println(key +" " +value);
+        }
+
+
+
+//        reduce
+        JavaRDD<Integer> parallelize = sc.parallelize(Arrays.asList(1, 2, 3, 4, 5));
+        Integer reduce = parallelize.reduce(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) throws Exception {
+                return integer+integer2;
+            }
+        });
+        System.out.println(reduce);
+
+//        zipWithIndex将RDD中元素与RDD的索引下标压缩成k，v格式的RDD
+        JavaPairRDD<Tuple2<String, String>, Long> tuple2LongJavaPairRDD = rdd.zipWithIndex();
+
 //        groupByKey将相同的key分组
         JavaPairRDD<String, Iterable<String>> groupByKey = rdd.groupByKey();
         groupByKey.foreach(new VoidFunction<Tuple2<String, Iterable<String>>>() {
